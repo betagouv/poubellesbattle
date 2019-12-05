16c311ed8cfb1c584dc1f9ddb8d45dacf6c8eb4d
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-12-05 17:03:15 +0100

    seed + index

--- app/controllers/donverts_controller.rb
+++ app/controllers/donverts_controller.rb
@@ -1,4 +1,6 @@
 class DonvertsController < ApplicationController
+skip_before_action :authenticate_user!
+
   def new
     @don = Donvert.new
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ require 'csv'
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Composteur.destroy_all
+Donvert.destroy_all
 # Composteur.create(address: '11 avenue Jean Mermoz, Pau', name: 'CC4-ARCENCIEL-PAU')
 # Composteur.create(address: '3 rue du Pin, Pau', name: 'CC7-MIDI-PAU')
 # Composteur.create(address: '73 boulevard Tourasse, Pau', name: 'CC19-TOURASSE-PAU')
@@ -27,3 +28,35 @@ CSV.foreach(filepath, csv_options) do |row|
     referent_full_name: row[6])
 end
 
+Donvert.create(
+  title: "Don de matière sèche chêne",
+  type_matiere_orga: "branchage",
+  description: "branchage de chênes et feuillus",
+  volume_litres: 45,
+  donneur_name: "Jardin collectif Magique",
+  donneur_address: "14, avenue de Tarbes, Pau, France",
+  donneur_tel: "0989098768",
+  donneur_email: "jardin@magique.com",
+  date_fin_dispo: "14/01/2020")
+
+Donvert.create(
+  title: "Don de matière sèche bananier",
+  type_matiere_orga: "branchage",
+  description: "branchage de bananier",
+  volume_litres: 36,
+  donneur_name: "Jardin collectif Magique",
+  donneur_address: "79, avenue de Tarbes, Pau, France",
+  donneur_tel: "0989098768",
+  donneur_email: "jardin@magique.com",
+  date_fin_dispo: "01/01/2020")
+
+Donvert.create(
+  title: "Don de compost",
+  type_matiere_orga: "compost",
+  description: "compost mûr",
+  volume_litres: 78,
+  donneur_name: "Composteur carlito",
+  donneur_address: "25, avenue de Lons, Pau, France",
+  donneur_tel: "0989098456",
+  donneur_email: "compo@carli.com",
+  date_fin_dispo: "09/12/2019")
