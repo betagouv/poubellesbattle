dca13e841ecc4c68b00bc4854818782d35a97beb
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-12-05 14:23:08 +0100

    controller and crud method ok for donverts

--- app/controllers/donverts_controller.rb
+++ app/controllers/donverts_controller.rb
@@ -1,13 +1,29 @@
 class DonvertsController < ApplicationController
   def new
+    @don = Donvert.new
   end
 
   def edit
   end
 
   def create
+    @don = Donvert.new(don_params)
+    @don.save
+    render :index
   end
 
   def index
+    @dons = Donvert.all
   end
+
+  private
+
+  def set_don
+    @don = Donvert.find(params[:id])
+  end
+
+  def don_params
+    params.require(:donvert).permit(:title, :type_matiere_orga, :volume_litres, :donneur_name, :donneur_address, :donneur_tel, :donneur_email, :date_fin_dispo, :description)
+  end
+
 end
