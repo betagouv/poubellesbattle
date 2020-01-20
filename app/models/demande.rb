7f67e5d45d677adfba59cedf10115fa19d1bfa32
paul lahana <paul.lahana@gmail.com>
2020-01-20 18:42:57 +0100

    add new and edit form and a tiny admin board 🛹

--- app/models/demande.rb
+++ app/models/demande.rb
@@ -1,2 +1,4 @@
 class Demande < ApplicationRecord
+  validates :first_name, :last_name, :email, :phone_number, :address, :logement_type, :inhabitant_type, presence: true
+  validates_inclusion_of :potential_users, in: [true, false]
 end
