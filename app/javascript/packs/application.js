000f06a0b33a8052bd4e176a9cc70bd5e000a09c
paul lahana <paul.lahana@gmail.com>
2020-03-04 12:29:37 +0100

    fixing mapbox ?

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,8 @@ import $ from 'jquery';
 import "../plugins/flatpickr"
 
 // importing mapbox
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import 'mapbox-gl/src/css/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import '@mapbox/mapbox-gl-geocoder/lib/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
 // import { initAutocomplete } from '../plugins/init_autocomplete';
 
