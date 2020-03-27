d3b93efb324e5a14d7c44d6993f6c9be59a09a75
paul lahana <paul.lahana@gmail.com>
2020-03-27 16:17:21 +0100

    fixing user edit + some show composteur fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :role, :composteur_id, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :role, :composteur_id, :photo, :ok_phone, :ok_mail])
   end
 end
