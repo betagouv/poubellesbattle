86e9f23069053b653d527830682073f8f45779f7
paul lahana <paul.lahana@gmail.com>
2020-04-15 12:52:33 +0200

    stats page visible to admins only

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def stats
+    return unless current_user.role == "admin"
     @users_count = User.count
   end
 end
