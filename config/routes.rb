aa1dd210832ea02b0cdcc1774579805085d4a052
paul lahana <paul.lahana@gmail.com>
2020-04-15 12:52:26 +0200

    stats page visible to admins only

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  post "pages/destroy_user/:id", to: "pages#destroy_user", as: 'destroy_user'
 
   resources :composteurs do
     member do
@@ -34,5 +33,6 @@ Rails.application.routes.draw do
   get "bourse_verte/:slug/link", to: "donverts#link", as: "link"
 
   resources :messages, only: [:new, :create]
-  get "pages/home", to: "pages#home", as: "home"
+
+  get "stats", to: "pages#stats", as: 'stats'
 end
