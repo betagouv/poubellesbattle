3db607e6624138fe275a37962050a9ca341f05d4
paul lahana <paul.lahana@gmail.com>
2020-04-28 13:06:49 +0200

    all stats are chartjs now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,24 +29,42 @@ class PagesController < ApplicationController
     # nombre d'inscrits sur la plateforme
     users = User.all
     @stats = []
-    @users_count = users.count
+    users_count = users.count
     users_last_month = users.created_before(last_month).count
     users_last_last_month = users.created_before(last_last_month).count
-    users_evol = (((@users_count.to_f - users_last_month.to_f)/ @users_count.to_f) * 100).round
+    users_evol = (((users_count.to_f - users_last_month.to_f)/ users_count.to_f) * 100).round
 
-    @next_month = I18n.t("date.month_names")[(Date.today + 1.month).month]
-    @this_month_full = I18n.t("date.month_names")[Date.today.month]
+    next_month = I18n.t("date.month_names")[(Date.today + 1.month).month]
+    this_month_full = I18n.t("date.month_names")[Date.today.month]
     last_month_full = I18n.t("date.month_names")[(Date.today - 1.month).month]
     last_last_month_full = I18n.t("date.month_names")[(Date.today - 2.month).month]
     ## Trying chartjs
     # Options for all charts
     @options = {
       # id: "chart_composteurs",
+      scales: {
+        yAxes: [{
+          id: 'first-y-axis',
+          type: 'linear',
+          ticks: { stepSize: 5 }
+        }]
+      },
       legend: {
-        display: false,
+        display: false
       },
       height: 200,
-      width: 900
+      width: 660
+    }
+    @options_line_bar = {
+      # id: "chart_composteurs",
+      tooltips: {
+        afterBody: "litres"
+      },
+      legend: {
+        display: false
+      },
+      height: 200,
+      width: 660
     }
     @options_doughnut = {
       # id: "chart_composteurs",
@@ -56,36 +74,46 @@ class PagesController < ApplicationController
     }
     #users chart
     @users_stats = {
-      labels: ["#{last_last_month_full}", "#{last_month_full}", "#{@this_month_full}"],
+      labels: ["#{last_last_month_full}", "#{last_month_full}", "#{this_month_full}"],
       datasets: [
         {
-          backgroundColor: "transparent",
-          borderColor: "red",
-          data: [users_last_last_month, users_last_month, @users_count]
+          backgroundColor: "#986535",
+          borderColor: "#4FB571",
+          borderWidth: 7,
+          pointBorderColor: "#986535",
+          pointBackgroundColor: "#FFC65A",
+          pointRadius: 7,
+          pointBorderWidth: 4,
+          data: [users_last_last_month, users_last_month, users_count]
         }
       ]
     }
     # composteurs chart
     composteurs = Composteur.all
     # nombre de nouveau composteur since 01-01-2020
-    @composteurs_count = composteurs.count
+    composteurs_count = composteurs.count
     # # evolution since premier seed == 53
     composteurs_last_month = composteurs.created_before(last_month).count
     composteurs_last_last_month = composteurs.created_before(last_last_month).count
-    composteurs_evol = (((@composteurs_count - composteurs_last_last_month)/ @composteurs_count.to_f) * 100).round
+    composteurs_evol = (((composteurs_count - composteurs_last_last_month)/ composteurs_count.to_f) * 100).round
     # # moyenne nombre / site
-    users_per_composteur = @users_count.fdiv(@composteurs_count).round(2)
+    users_per_composteur = users_count.fdiv(composteurs_count).round(2)
 
     @composteurs_stats = {
-      labels: ["#{last_last_month_full}", "#{last_month_full}", "#{@this_month_full}",],
+      labels: ["#{last_last_month_full}", "#{last_month_full}", "#{this_month_full}",],
       datasets: [
-          {
-            backgroundColor: "transparent",
-            borderColor: "blue",
-            data: [composteurs_last_last_month, composteurs_last_month, @composteurs_count]
-          }
-        ]
-      }
+        {
+          backgroundColor: "#986535",
+          borderColor: "#4FB571",
+          borderWidth: 7,
+          pointBorderColor: "#986535",
+          pointBackgroundColor: "#FFC65A",
+          pointRadius: 7,
+          pointBorderWidth: 4,
+          data: [composteurs_last_last_month, composteurs_last_month,composteurs_count]
+        }
+      ]
+    }
 
     notifications = Notification.all
     # # nombre d'anomalies total
@@ -110,21 +138,54 @@ class PagesController < ApplicationController
         }
       ]
     }
-    # # nombre d'annonces publiees sur la bourse verte
-    donverts = Donvert.all
-    donverts_count = donverts.count
-    # # volume de matiere echangee ? annonces pourvues x volume par annonce ?
-    volume_total = 0
-    donverts.where.not(type_matiere_orga: "outils").each do |don|
-      don.pourvu ? volume_total += don.volume_litres : volume_total
-    end
-    # # frequentation globale ? matomo api ?
-    # # nombre de demande de composteurs/mois
+
+
     demandes = Demande.all
     demandes_count = demandes.count
-    demandes_this_month = demandes.created_after(last_month).count
-    # # ratio demandes / nouveaux composteurs
-    ratio_demande_vs_new = ((composteurs.created_after(last_month).count / demandes_this_month) * 100).round
+    demandes_last_month = demandes.created_before(last_month).count
+    demandes_last_last_month = demandes.created_before(last_last_month).count
+
+    @demandes_stats = {
+      labels: ["#{last_last_month_full}", "#{last_month_full}", "#{this_month_full}",],
+      datasets: [
+        {
+          backgroundColor: "#986535",
+          borderColor: "#4FB571",
+          borderWidth: 7,
+          pointBorderColor: "#986535",
+          pointBackgroundColor: "#FFC65A",
+          pointRadius: 7,
+          pointBorderWidth: 4,
+          data: [demandes_last_last_month, demandes_last_month, demandes_count]
+        }
+      ]
+    }
+    # # nombre d'annonces publiees sur la bourse verte
+    donverts = Donvert.all
+    @donverts_count = donverts.count
+    # # volume de matiere echangee ? annonces pourvues x volume par annonce ?
+    volume_compost = 0
+    volume_structurant = 0
+    donverts.where.not(type_matiere_orga: "outils").each do |don|
+      if don.pourvu
+        don.type_matiere_orga == "compost" ? volume_compost += don.volume_litres : volume_compost
+        don.type_matiere_orga == "structurant" ? volume_structurant += don.volume_litres : volume_structurant
+      end
+    end
+    @volume_total = volume_compost + volume_structurant
+    @volume_stats = {
+      labels: ["Compost", "Structurant"],
+      datasets: [
+        {
+          backgroundColor: ["#4FB571", "#986535"],
+          data: [volume_structurant, volume_compost]
+        }
+      ]
+    }
+
+    # # frequentation globale ? matomo api ?
+
+    # ratio_demande_vs_new = ((composteurs.created_after(last_month).count / demandes_this_month) * 100).round
     # # nombre de notifications total
 
     ## building @stats
@@ -132,18 +193,18 @@ class PagesController < ApplicationController
     # @stats << ["number", "Nombre d'inscrit•e•s le mois dernier", users_last_month]
     # @stats << ["percent large-card", "Progression des inscriptions depuis le mois dernier", users_evol]
     # @stats << ["number", "Nombre de d'inscrit•e•s par site de compostage", users_per_composteur]
-    # @stats << ["number", "Nombre de sites de compostage", @composteurs_count]
+    # @stats << ["number", "Nombre de sites de compostage", composteurs_count]
     # @stats << ["number", "Nombre de sites le mois dernier", composteurs_last_month]
     # @stats << ["number large-card", "Progression du nombre de site de compostage", composteurs_evol]
-    @stats << ["number", "Nombre d'annonces publiées sur la Bourse Verte", donverts_count]
-    @stats << ["volume", "Volume total de matière échangée", "#{volume_total} L"]
-    @stats << ["number", "Nombre total de demandes d'installation", demandes_count]
-    @stats << ["number", "Nombre de demandes d'installation ce mois", demandes_this_month]
-    @stats << ["percent", "Ratio de demandes transformées en site de compostage", ratio_demande_vs_new]
-    @stats << ["number", "Nombre d'anomalie signalée sur les sites de compostage", anomalies_count]
-    @stats << ["number", "Nombre d'anomalies résolues", resolved_anomalies_count]
-    @stats << ["number", "Nombre de dépôts effectués", depots_count]
-    @stats << ["number", "Nombre de messages échangés par la communauté Poubelles Battle", messages_count]
+    # @stats << ["number", "Nombre d'annonces publiées sur la Bourse Verte", @donverts_count]
+    # @stats << ["volume", "Volume total de matière échangée", "#{volume_total} L"]
+    # @stats << ["number", "Nombre total de demandes d'installation", demandes_count]
+    # @stats << ["number", "Nombre de demandes d'installation ce mois", demandes_this_month]
+    # @stats << ["percent", "Ratio de demandes transformées en site de compostage", ratio_demande_vs_new]
+    # @stats << ["number", "Nombre d'anomalie signalée sur les sites de compostage", anomalies_count]
+    # @stats << ["number", "Nombre d'anomalies résolues", resolved_anomalies_count]
+    # @stats << ["number", "Nombre de dépôts effectués", depots_count]
+    # @stats << ["number", "Nombre de messages échangés par la communauté Poubelles Battle", messages_count]
   end
 
   def annuaire
