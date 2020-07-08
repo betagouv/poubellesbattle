de70977a44e23e357513f7f0c7c6f73291928933
paul lahana <paul.lahana@gmail.com>
2020-07-08 16:44:16 +0200

    requests for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,11 @@
 class MessagesController < ApplicationController
   require 'uri'
 
-  def new
-    @message = Message.new
-  end
-
   def create
     @message = Message.new(message_params)
-
+    if @message.message_type == "message-membres" || @message.message_type == "message-agglo"
+      redirect_to root_path and return unless current_user.referent?
+    end
     if @message.save
       flash[:notice] = "Votre message a été envoyé"
     else
@@ -20,11 +18,15 @@ class MessagesController < ApplicationController
       redirect_to composteur_path(current_user.composteur)
     elsif @message.message_type == 'message-to-referent'
       @referent_composteur = User.find(@message.recipient_id.to_i)
-      case URI(request.referer).path
-      when '/'
+      unless request.referer
         redirect_to root_path
-      when '/composteurs/' + @referent_composteur.composteur.id.to_s
-        redirect_to composteur_path(@referent_composteur.composteur)
+      else
+        case URI(request.referer).path
+        when '/'
+          redirect_to root_path
+        when '/composteurs/' + @referent_composteur.composteur.id.to_s
+          redirect_to composteur_path(@referent_composteur.composteur)
+        end
       end
     end
   end
