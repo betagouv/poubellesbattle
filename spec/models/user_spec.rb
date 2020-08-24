02425c70c6a053d91d05ad05b59b851991e2f65e
paul lahana <paul.lahana@gmail.com>
2020-08-24 10:54:11 +0200

    feature tests and model tests for user/devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) { @user = create(:user, password: 'password123', password_confirmation: 'password123') }
+  it 'is database authenticable' do
+    # user = create(:user, password: 'password123', password_confirmation: 'password123')
+    expect(@user.valid_password?('password123')).to be_truthy
+  end
+  it 'rejects too short password' do
+    # user = create(:user)
+    @user.password = 'abc'
+    expect(@user).to_not be_valid
+  end
+  it 'sends welcome email' do
+    expect { @user.instance_eval { send_welcome_email } }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    expect(ActionMailer::Base.deliveries.last.subject).to eq('Bienvenue sur Poubelles Battle')
+    expect(ActionMailer::Base.deliveries.last.to.first).to eq(@user.email)
+  end
 end
