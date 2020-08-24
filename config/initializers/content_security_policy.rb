8b6bdbb4720d060bc87c342fd7efb4d4c2b2c079
paul lahana <paul.lahana@gmail.com>
2020-08-24 17:43:50 +0200

    security headers : in progress

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,18 +3,27 @@
 # Define an application-wide content security policy
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+unless Rails.env.test?
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+  Rails.application.config.content_security_policy do |policy|
+    policy.default_src :self, :data, :blob, "fonts.gstatic.com", "*.crisp.chat", "crisp.chat", "*.crisp.help", 'https://*.tiles.mapbox.com', 'https://api.mapbox.com', 'https://events.mapbox.com'
+    policy.font_src    :self, :data, 'fonts.gstatic.com', '*.bootstrapcdn.com', "*.crisp.chat", "crisp.chat"
+    policy.img_src     :self, :data, :blob, '*.cloudinary.com', "*.openstreetmap.org", "stats.data.gouv.fr", "*.crisp.chat", "crisp.chat"
+    policy.worker_src  :self, :blob
+    policy.object_src  :none
+    policy.style_src   :self, :unsafe_inline, 'fonts.googleapis.com', '*.bootstrapcdn.com', '*.mapbox.com', '*.mailchimp.com', "*.crisp.chat", "crisp.chat"
+    if Rails.env.development?
+      policy.script_src  :self, :unsafe_inline, "localhost:3000", "stats.data.gouv.fr", "*.crisp.chat", "crisp.chat", "*.mapbox.com"
+      policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035', 'ws://localhost:3000', "wss://*.crisp.chat", "*.crisp.chat", 'https://*.tiles.mapbox.com', 'https://api.mapbox.com', 'https://events.mapbox.com'
+    else
+      policy.script_src  :self, :unsafe_inline, "stats.data.gouv.fr", "*.crisp.chat", "crisp.chat", "*.mapbox.com"
+      policy.connect_src :self, "wss://*.crisp.chat", "*.crisp.chat", 'https://*.tiles.mapbox.com', 'https://api.mapbox.com', 'https://events.mapbox.com'
+    end
+    # Specify URI for violation reports
+    # policy.report_uri "/csp-violation-report-endpoint"
+  end
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
