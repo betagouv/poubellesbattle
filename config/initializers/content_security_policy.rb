37883c46afb4cb6146743dc93ad348bf70c47f9d
paul lahana <paul.lahana@gmail.com>
2020-09-11 10:52:38 +0200

    seo-and-csp-fixes

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -8,16 +8,16 @@ unless Rails.env.test?
   Rails.application.config.content_security_policy do |policy|
     policy.default_src :self
     policy.font_src    :self, :data, 'fonts.gstatic.com', 'bootstrapcdn.com', 'maxcdn.bootstrapcdn.com', 'crisp.chat', 'client.crisp.chat'
-    policy.img_src     :self, :data, :blob, 'cloudinary.com', 'openstreetmap.org', 'stats.data.gouv.fr', 'crisp.chat', 'crisp.chat', 'https://image.crisp.chat/'
+    policy.img_src     :self, :data, :blob, 'cloudinary.com', 'openstreetmap.org', 'stats.data.gouv.fr', 'crisp.chat', 'https://image.crisp.chat/', 'https://client.crisp.chat/'
     policy.worker_src  :self, :blob
     policy.object_src  :none
     policy.frame_src   'https://compostable-ou-non.now.sh/'
     policy.style_src   :self, :unsafe_inline, 'fonts.googleapis.com', 'maxcdn.bootstrapcdn.com/', 'bootstrapcdn.com', 'mapbox.com', 'mailchimp.com','cdn-images.mailchimp.com', 'crisp.chat', 'client.crisp.chat'
     if Rails.env.development?
-      policy.script_src  :self, 'script_dynamic', 'http://localhost:3000', 'stats.data.gouv.fr', 'mapbox.com', 'crisp.chat', 'client.crisp.chat', 'https://stats.data.gouv.fr/piwik.js', 'https://settings.crisp.chat'
+      policy.script_src  :self, 'http://localhost:3000', 'stats.data.gouv.fr', 'mapbox.com', 'crisp.chat', 'client.crisp.chat', 'https://stats.data.gouv.fr/piwik.js', 'https://settings.crisp.chat', 'https://client.crisp.chat/'
       policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035', 'ws://localhost:3000', 'wss://crisp.chat', 'wss://client.relay.crisp.chat', 'crisp.chat', 'client.crisp.chat', 'https://tiles.mapbox.com', 'https://api.mapbox.com', 'https://events.mapbox.com'
     else
-      policy.script_src  :self, 'stats.data.gouv.fr', 'crisp.chat', 'crisp.chat', 'mapbox.com', 'https://client.crisp.chat/l.js', 'https://settings.crisp.chat', 'https://stats.data.gouv.fr/piwik.js', 'https://client.crisp.chat/static/javascripts/client.js'
+      policy.script_src  :self, 'stats.data.gouv.fr', 'crisp.chat', 'crisp.chat', 'mapbox.com', 'https://client.crisp.chat/', 'https://settings.crisp.chat', 'https://stats.data.gouv.fr/piwik.js', 'https://client.crisp.chat/static/javascripts/'
       policy.connect_src :self, 'wss://crisp.chat', 'wss://client.relay.crisp.chat', 'crisp.chat', 'https://tiles.mapbox.com', 'https://api.mapbox.com', 'https://events.mapbox.com'
     end
     # Specify URI for violation reports
