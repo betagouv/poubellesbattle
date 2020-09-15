22c4c8f8f1538885f7a8753c8f2756db60f8a9dd
paul lahana <paul.lahana@gmail.com>
2020-09-15 14:54:08 +0200

    adding data confirm modal js and adapting all data confirm

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,13 @@ const { environment } = require('@rails/webpacker')
 
 environment.loaders.delete('nodeModules')
 
+const webpack = require('webpack')
+
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    jQuery: 'jquery',
+  })
+)
+
 module.exports = environment
