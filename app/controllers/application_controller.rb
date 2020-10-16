b385567ede6c83f260cebee144490bc41ab05fd9
paul lahana <paul.lahana@gmail.com>
2020-10-16 10:26:17 +0200

    removing ensure domain method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_action :ensure_domain
+  # before_action :ensure_domain
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
@@ -30,13 +30,13 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def ensure_domain
-    return unless Rails.env.production?
+  # def ensure_domain
+  #   return unless Rails.env.production?
 
-    return unless request.env['HTTP_HOST'] != 'agglo-pau.fr'
+  #   return unless request.env['HTTP_HOST'] != 'agglo-pau.fr'
 
-    redirect_to root_path, status: 301
-  end
+  #   redirect_to root_path, status: 301
+  # end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
