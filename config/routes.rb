20886ff031afd1e1d79c1dfd7bc02058c999af66
paul lahana <paul.lahana@gmail.com>
2020-11-04 11:52:50 +0100

    adding recaptcha

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
 
   match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'} if Rails.env.production?
 
